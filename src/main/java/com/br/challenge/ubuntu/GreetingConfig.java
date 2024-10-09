aa515c1f0fff25c9b0cdb18818d000abfd694c12
Matheus Fideles <matheuss.fideles@hotmail.com>
2024-10-08 22:59:17 -0300

    initial commit

---
+++ src/main/java/com/br/challenge/ubuntu/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.br.challenge.ubuntu;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
